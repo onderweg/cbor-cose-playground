45e4235d8f6c11ae6abcda4b27648842d39fb9c4
gerwert <onderweg@users.noreply.github.com>
2021-11-26 15:46:06 +0100

    Fix wrong structure for MAC messages

--- cose-verify.c
+++ cose-verify.c
@@ -29,7 +29,7 @@ typedef struct cose_raw_msg
     bytes protected_header;
     bytes unprotected_header;
     bytes signature;   
-    bytes calculated_signature; 
+    bytes to_verify; 
 } cose_raw_msg;
 
 size_t hexstring_to_buffer(byte **buffer, char *string, size_t string_len)
@@ -64,7 +64,35 @@ size_t buffer_to_hexstring(char **string, byte *buffer, size_t buf_len)
 }
 
 /**
- * Does CBOR encoding of a signed message structure
+ * Encodes COSE MAC_structure structure message structure in CBOR.
+ * See: https://www.ietf.org/id/draft-ietf-cose-rfc8152bis-struct-15.html#name-how-to-compute-and-verify-a
+ */
+void cose_mac0_structure(const char *context,
+                          bytes *body_protected,
+                          bytes *external_aad,
+                          bytes *payload,
+                          uint8_t *out,
+                          size_t out_size,
+                          size_t *out_len)
+{
+    CborEncoder enc;    
+    cbor_encoder_init(&enc, out, out_size, 0);
+
+    CborEncoder ary;
+    cbor_encoder_create_array(&enc, &ary, 4);
+
+    cbor_encode_text_stringz(&ary, context);
+    cbor_encode_byte_string(&ary, body_protected->buf, body_protected->len);
+    cbor_encode_byte_string(&ary, external_aad->buf, external_aad->len);
+    cbor_encode_byte_string(&ary, payload->buf, payload->len);
+
+    cbor_encoder_close_container(&enc, &ary);
+    *out_len = cbor_encoder_get_buffer_size(&enc, out); 
+}
+
+/**
+ * Encodes COSE Sig_structure structure in CBOR.
+ * See: https://www.ietf.org/id/draft-ietf-cose-rfc8152bis-struct-15.html#name-signing-and-verification-pr
  */
 void cose_sign1_structure(const char *context,
                           bytes *body_protected,
@@ -129,12 +157,12 @@ void cose_parse_protected_hdr(bytes *protected, cose_protected_header *out)
 /**
  * Compare calculated HMAC 256 signature with received signature
  */
-int verify_hmac(bytes *to_verify, bytes *signature, char *secret)
+int verify_hmac(bytes *to_verify, bytes *signature, size_t secret_len, const byte *secret)
 {
     Hmac hmac;
     byte hmacDigest[SHA256_DIGEST_SIZE];
 
-    wc_HmacSetKey(&hmac, WC_SHA256, (const byte *)secret, strlen(secret));
+    wc_HmacSetKey(&hmac, WC_SHA256, secret, secret_len);
 
     wc_HmacUpdate(&hmac, to_verify->buf, to_verify->len);
     wc_HmacFinal(&hmac, hmacDigest);
@@ -145,7 +173,7 @@ int verify_hmac(bytes *to_verify, bytes *signature, char *secret)
 
     char *y;
     buffer_to_hexstring(&y, signature->buf, signature->len);
-    printf("Received Signature: %s\n", y);
+    printf("Embeded Signature: %s\n", y);
 
     int ret = memcmp(hmacDigest, signature->buf, SHA256_DIGEST_SIZE);
     return ret;
@@ -167,7 +195,7 @@ int verify_es256(bytes *to_verify, bytes *signature, ecc_key *key)
 }
 
 /**
- * Decode sign1 message and calculate expected signature
+ * Decode sign1 message and calculate bytes to be verified
  */
 void cose_decode_sign1(bytes *sign1, uint8_t *calculated_sig_buf, size_t calculated_sig_size, cose_raw_msg *out)
 {
@@ -202,39 +230,59 @@ void cose_decode_sign1(bytes *sign1, uint8_t *calculated_sig_buf, size_t calcula
     bytes payload;    
     cbor_value_dup_byte_string(&e, &payload.buf, &payload.len, &e);
 
-    // Get signature
+    // Get signature (sign1) or tag (mac0)
     bytes signature;
     cbor_value_dup_byte_string(&e, &signature.buf, &signature.len, &e);
 
-    // Calculate signature. This calculated signature will be used to verify message.  
-    size_t to_verify_len;
-    cose_sign1_structure(
-        "Signature1", 
-        &protected, 
-        &unprotected, 
-        &payload, 
-        calculated_sig_buf, 
-        calculated_sig_size, 
-        &to_verify_len
-    );
+    // Calculate bytes to verify. 
+    size_t to_verify_len = 0;
+    if (tag == 18) {
+        // sign 1
+        cose_sign1_structure(
+            "Signature1", 
+            &protected, 
+            &unprotected, 
+            &payload, 
+            calculated_sig_buf, 
+            calculated_sig_size, 
+            &to_verify_len
+        );
+    } else if (tag == 17) {
+        // mac0
+        bytes external_aad ={NULL, 0};
+        cose_mac0_structure(
+            "MAC0", 
+            &protected, 
+            &external_aad, 
+            &payload,
+            calculated_sig_buf, 
+            calculated_sig_size, 
+            &to_verify_len
+        );
+    }
     bytes to_verify = (bytes){calculated_sig_buf, to_verify_len};
 
     out->payload = payload;
     out->protected_header = protected;
     out->unprotected_header = unprotected;
     out->signature = signature;
-    out->calculated_signature = to_verify;
+    out->to_verify = to_verify;
 }
 
 int main(int argc, char *argv[])
 {
     cose_raw_msg signed_msg;
-    byte *msg_buf;
+    byte *msg_buf, *key_buf;
 
-    // HMAC-SHA256 signed COSE message    
-    char *msg_hex = "d28445a201050300a04e546869732061206d657373616765582091e726b7d4897fdfdfff50652d977fdd3dbe3110d08059569ffbfa18978b281e";    
+    // Example HMAC-SHA256 signed COSE message    
+    // Source: // Source: https://github.com/cose-wg/Examples/blob/3221310e2cf50ad13213daa7ca278209a8bc85fd/mac0-tests/HMac-01.json
+    char *msg_hex = "D18443A10105A054546869732069732074686520636F6E74656E742E5820A1A848D3471F9D61EE49018D244C824772F223AD4F935293F1789FC3A08D8C58";    
+    char *key_hex = "849B57219DAE48DE646D07DBB533566E976686457C1491BE3A76DCEA6C427188";
 
+    // Convert message and key hex strings to bytes
+    size_t key_len = hexstring_to_buffer(&key_buf, key_hex, strlen(key_hex));
     size_t msg_len = hexstring_to_buffer(&msg_buf, msg_hex, strlen(msg_hex));
+
     bytes msg_bytes = {msg_buf, msg_len};
     uint8_t to_verify_buf[1024];
     cose_decode_sign1(&msg_bytes, to_verify_buf, sizeof(to_verify_buf), &signed_msg);   
@@ -246,12 +294,12 @@ int main(int argc, char *argv[])
 
     printf("Signature type in protected header: %i\n", protected_header.alg);
     if (protected_header.alg == COSE_ALG_HMAC_256)
-    {
-        char* key = "vleuten";
+    {        
         int verified = verify_hmac(
-            &signed_msg.calculated_signature,
+            &signed_msg.to_verify,
             &signed_msg.signature,
-            key);
+            key_len,
+            key_buf);
         printf("Verified: %s\n", verified == 0 ? "YES" : "NO");
     }
 }
